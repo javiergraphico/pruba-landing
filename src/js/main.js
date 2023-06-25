7473f2e59dc0578925cf894f93d20865f0f4e5b0
javiergraphico <javieralexander38@hotmail.com>
2023-06-25 00:18:03 -0400

    prueba

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+
+
+
+ScrollReveal().reveal('.navbar', { 
+  delay: 500,
+  origin: 'bottom',
+  distance: '-100px',
+  mobile: false
+ });
+
+ ScrollReveal().reveal('.creative', { 
+  mobile: true,
+  delay: 1000,
+  origin: 'right',
+  distance: '-100px',
+  
+ });
+
+ ScrollReveal().reveal('.visual', { 
+  delay: 2000,
+  origin: 'left',
+  distance: '-500px',
+  mobile: true
+ });
+
+ ScrollReveal().reveal('.social-media', { 
+  delay: 1200,
+  origin: 'top',
+  easing: 'ease-in-out',
+  distance: '-100px',
+  mobile: true
+
+ });
+
+
+
+
